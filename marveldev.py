bdd7fdacda2931f0461b9e1282f189a8e81b7e65
Mayte Giménez <mgimenez@dsic.upv.es>
2014-08-07 14:00:33 +0200

    It manages comic vine api credentials

--- marveldev.py
+++ marveldev.py
@@ -6,6 +6,7 @@ class Developer(object):
     def __init__(self):
         _public_key = None
         _private_key = None
+        _comicvine_key = None
         self.load_keys()
         
     def load_keys(self):
@@ -16,9 +17,13 @@ class Developer(object):
         # Get keys
         self._public_key = config.get('Marvel Keys', 'public')
         self._private_key = config.get('Marvel Keys', 'private')
-
-    def get_credentials(self):
+        self._comicvine_key = config.get('Comic Vine', 'key')
+        
+    def get_marvel_credentials(self):
         """ Returns a pair with marvel developer credentials """
         return (self._public_key, self._private_key)
 
+    def get_comicvine_credentials(self):
+        """ Returns a pair with marvel developer credentials """
+        return (self._comicvine_key)
 
